e4e391d19570ce57f915afb7c82db44f016bacc5
Krantz-XRF <Krantz.XRF@outlook.com>
2019-09-12 13:37:48 +0800

    格式修改：使用 $ 运算符来消除一对括号

--- app/Main.hs
+++ app/Main.hs
@@ -20,11 +20,11 @@ main = do
     print (runDfa dfa str)
 
 dfa :: Dfa Char Token
-dfa = $(lift (minimize $ determine $ buildNfa $ mapM (mapM buildRegex)
+dfa = $(lift $ minimize $ determine $ buildNfa $ mapM (mapM buildRegex)
     [ (Type,        Range 'A' 'Z' `Concat` Many (Range 'A' 'Z' `Or` Range 'a' 'z'))
     , (Variable,    (Range 'a' 'z' `Or` sng '_') `Concat` Some (Range 'A' 'Z' `Or` Range 'a' 'z'))
     , (Decimal,     Range '1' '9' `Concat` Some (Range '0' '9'))
     , (Octal,       sng '0' `Concat` Many (Range '0' '7'))
     , (Hexadecimal, sng '0' `Concat` (sng 'x' `Or` sng 'X') `Concat`
                     Some (Range '0' '9' `Or` Range 'A' 'F' `Or` Range 'a' 'z'))
-    ]))
+    ])
